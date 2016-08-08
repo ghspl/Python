c81b088cbca3c9259c2ab850639e83ab5e0b740f
spl <1274471576@qq.com>
2016-08-08 20:25:52 +0800

    basic

---
+++ Chap2.py
@@ -0,0 +1,68 @@
+# print "Hello World!"
+# myString = 'Hello World!'
+# print myString
+
+# print "%s is number %d!" % ("Python",1)
+
+# user = raw_input('Enter login name:')
+# print 'Your login is:',user
+
+# num = raw_input('Now enter a number:')
+# print 'Doubling your number: %d' % (int(num) * 2)
+
+# help(raw_input)
+
+#print -2 * 4 + 3 ** 2
+
+# counter = 0
+# miles = 1000.0
+# name = 'Bob'
+# counter = counter + 1
+# kilometers = 1.609 * miles
+# print '%f miles is the same as %f km' % (miles, kilometers)
+
+# print 'I like to use the Internet for:'
+# for item in['e-mail', 'net-surfing', 'homework', 'chat']:
+#     print item,
+#     print
+
+# who = 'knights'
+# what = 'Ni!'
+# print 'We are the', who, "who say", what, what, what, what
+# print 'We are the %s who say %s' %\
+#       (who, ((what + ' ')*4))
+
+# for eachNum in [0, 1, 2]:
+#     print eachNum
+
+# foo = 'abc'
+# for c in foo:
+#     print c
+
+# foo = 'abc'
+# for i in range(len(foo)):
+#     print foo[i], '(%d)' % i
+
+# foo = 'abc'
+# for i, ch in enumerate(foo):
+#     print ch, '(%d)' % i
+
+# squared = [x ** 2 for x in range(4)]
+# for i in squared:
+#     print i
+
+# sqdEvens = [x ** 2 for x in range(8) if not x % 2]
+# for i in sqdEvens:
+#     print i
+
+# aList = [1,2,3,4]
+# print aList
+# print aList[0]
+# print aList[2:]
+# print aList[:3]
+# aList[1] = 5
+# print aList
+
+# for eachNum in [0,1,2]:
+#     print eachNum
+
